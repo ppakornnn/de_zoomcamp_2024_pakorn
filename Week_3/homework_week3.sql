3e7861c0953af1ce67e40a9781714d05b4c73f8b
ppakornnn <155470233+ppakornnn@users.noreply.github.com>
2024-02-12 18:07:08 +0700

    Edit HW3

--- Week_3/homework_week3.sql
+++ Week_3/homework_week3.sql
@@ -5,6 +5,20 @@ SELECT
 
 FROM `terraform-demo-411906.ny_taxi.green_taxi_data`
 ;
+-- Question 2 --
+--Materialzed Table --
+SELECT
+   COUNT(DISTINCT PULocationID)
+
+FROM `terraform-demo-411906.ny_taxi.green_taxi_data`
+;
+--External Table --
+SELECT
+   COUNT(DISTINCT PULocationID)
+
+FROM `terraform-demo-411906.ny_taxi.green_taxi_ext_table`
+;
+
 -- Question 3 --
 
 SELECT
@@ -15,10 +29,19 @@ WHERE
    TRUE
    AND fare_amount = 0
 ;
--- Question 5 --
+
+-- Question 4 --
 
 CREATE OR REPLACE TABLE `ny_taxi.green_taxi_data_partition` 
 PARTITION BY DATE(lpep_pickup_datetime) AS
 SELECT *
 FROM `ny_taxi.green_taxi_data`
+;
+
+-- Question 5 --
+SELECT
+      COUNT(DISTINCT PULocationID)
+FROM `ny_taxi.green_taxi_data_partition`
+WHERE
+   lpep_pickup_datetime BETWEEN '2022-06-01' AND '2022-06-30'
 ;
